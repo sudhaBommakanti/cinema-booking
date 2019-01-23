5d37ac3b2828430ea40428070bfd65d794d07d8d
Joel <joel.pedersen97@gmail.com>
2019-01-23 10:07:48 +0100

    Revert "Added MongoDB Atlas"
    This reverts commit 048c4a63a1eeb1ed6d65808910e1ecab729a2931.

--- www/js/test.js
+++ www/js/test.js
@@ -1,6 +1,31 @@
+async function runTests() {
+
+  // Create a new author
+  let raymond = new Author({
+    name: 'Raymond Chandler',
+    description: 'Chandler was one of the fathers of crime noire.'
+  });
+
+  // Save
+  console.log(await raymond.save());
+
+  // Update an existing author
+  let shakespeare = await Author.find(`.findOne({name: /Shake/})`);
+  shakespeare.description += ' And even greater...';
+  console.log(await shakespeare.save());
+
+  // Find and then delete Samuel Beckett
+  let samuel = await Author.find(`.findOne({name: /Beckett/})`);
+  if (samuel) {
+    console.log(await samuel.delete());
+  }
+}
+
+/* runTests(); */
+
 async function runMovieTest() {
   let showtime = new Showtime({
-    auditorium: "Stora Salongen",
+    audiotorium: "Stora Salongen",
     film: "The Wife",
     date: "2019-01-23",
     time: "15:30"
@@ -9,3 +34,5 @@ async function runMovieTest() {
   console.log(await showtime.save());
 
 }
+
+runMovieTest();
