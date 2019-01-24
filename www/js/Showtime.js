1888a8e9ac5c3298d3505735233c62a4995bb892
Pontus Nilsson <p0nta_@hotmail.com>
2019-01-24 20:00:33 +0100

    Trying to add frontend to backend

--- www/js/Showtime.js
+++ www/js/Showtime.js
@@ -1,1 +1,12 @@
-class Showtime extends REST { }
+class Showtime extends REST {
+
+    createSshowtimes() {
+        for (let i = 0; i < 84; i++) {
+          //models.movies.find(); Loop thorugh
+          let movies = ["The Greatest Showman", "Bird Box", "A Star Is Born", "Me Before You", "Armageddon"];
+          let auditorium = ["VIP Salongen", "Stora Salongen", "Lilla Salongen"];
+    
+    
+        }
+      }
+ }
