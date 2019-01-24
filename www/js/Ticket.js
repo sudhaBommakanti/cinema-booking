4ae738c79e871c3cea7e20b7ae6a038862797344
Yasmin <minaa0396@gmail.com>
2019-01-24 15:23:59 +0100

    :)

--- www/js/Ticket.js
+++ www/js/Ticket.js
@@ -1,16 +1,15 @@
 class Ticket extends REST {
-  // constructor() {}
-  // async generateTicket() {
-  //   let ticket = new Ticket({
-  //     userNames: [String],
-  //     showtime: [{
-  //       type: Schema.Types.ObjectId,
-  //       ref: 'Showtime'
-  //     }],
-  //     seats: [Number],
-  //     prices: [Number]
-  //   });
-  //   console.log(await ticket.save());
+  async generateTicket() {
+    let ticket = new Ticket({
+      userNames: [String],
+      showtime: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Showtime'
+      }],
+      seats: [Number],
+      prices: [Number]
+    });
+   console.log(await ticket.save());
 
-  // }
+  }
 }
