ba947a4f90df05142828812bedf059b5a31ea1e2
Joel <joel.pedersen97@gmail.com>
2019-01-25 09:24:21 +0100

    Save showtimes to database

--- www/js/Showtime.js
+++ www/js/Showtime.js
@@ -1,5 +1,5 @@
 class Showtime extends REST {
-  createShowtimes() {
+  async createShowtimes() {
     let date = new Date();
     for (let i = 0; i < 84; i++) {
       let movies = ["The Greatest Showman", "Bird Box", "A Star Is Born", "Me Before You", "Armageddon"];
@@ -9,13 +9,13 @@ class Showtime extends REST {
         date.setDate(date.getDate() + 1);
       }
 
-      let showtime = {
+      let showtime = new Showtime({
         "auditorium": auditorium.splice(Math.floor(Math.random() * auditorium.length), 1),
         "film": movies.splice(Math.floor(Math.random() * movies.length), 1),
         "date": date,
-        "time": 17 + Math.floor(Math.random() * 3) + ':' + (Math.round(Math.random() < 0.5 ? 00 : 30))
-      }
-      console.log(showtime);
+        "time": 17 + Math.floor(Math.random() * 3) + ':' + (Math.round(Math.random() < 0.5 ? 15 : 45))
+      });
+      console.log(await showtime.save());
     }
   }
 }
--- www/js/test.js
+++ www/js/test.js
@@ -1,13 +1,3 @@
-function createShowtimes() {
-  for (let i = 0; i < 84; i++) {
-    let movies = ["The Greatest Showman", "Bird Box", "A Star Is Born", "Me Before You", "Armageddon"];
-    let auditorium = ["VIP Salongen", "Stora Salongen", "Lilla Salongen"];
-    let time = {
-      "auditorium": auditorium.splice(Math.floor(Math.random() * auditorium.length), 1),
-      "film": movies.splice(Math.floor(Math.random() * movies.length), 1),
-      "date": new Date(Date.now() + 24 * 60 * 60 * 1000),
-      "time": 17 + Math.floor(Math.random() * 3) + ':' + (Math.round(Math.random() < 0.5 ? 00 : 30))
-    }
-    console.log(await time);
-  }
-}
+let time = new Showtime();
+
+time.createShowtimes();
