37beb9814df8bb99c4ff7199a0ecd154f13e2c84
Fanny <fanny_andersson@hotmail.com>
2019-01-25 13:10:59 +0100

    Add a row function

--- www/js/Auditorium.js
+++ www/js/Auditorium.js
@@ -2,7 +2,14 @@ class Auditorium extends REST {
     static async generateAuditorium() {
         let smallAuditorium = new Auditorium({
             name: 'Lilla salongen',
-            seatsPerRow: [6, 8, 9, 10, 10, 12]
+            seatsPerRow: [
+                { row1: [6] },
+                { row2: [8] },
+                { row3: [9] },
+                { row4: [10] },
+                { row5: [10] },
+                { row6: [12] }
+            ]
         });
 
         let bigAuditorium = new Auditorium({
@@ -20,3 +27,16 @@ class Auditorium extends REST {
 }
 
 Auditorium.generateAuditorium();
+
+function getTheSeatsFromSmallAuditorium() {
+    let salon = smallAuditorium.name;
+    if (salon === 'Lilla salongen') {
+        for (let row = 0; i <= 6; i++) {
+            for (let seat = 0; seat <= 55; seat++) {
+                console.log(row + '-' + seat);
+            }
+        }
+    }
+}
+
+getTheSeatsFromSmallAuditorium();
