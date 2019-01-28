0b3b247b642956586d20547b313e9a924f8fdab9
Joel <joel.pedersen97@gmail.com>
2019-01-28 14:14:58 +0100

    yeye

--- www/js/test.js
+++ www/js/test.js
@@ -2,7 +2,7 @@ async function testLogin(){
  
   // credentials we want to login with
   let login = new Login({
-    email: 'joe@skibidi.com',
+    email: 'joel@skibidi.com',
     password: '123abc'
   });
  
@@ -23,60 +23,3 @@ async function testLogout(){
 testLogin();
 //testCheckLogin();
 // testLogout();
-
-async function runTests() {
-
-  // Create a new author
-  let raymond = new Author({
-    name: 'Raymond Chandler',
-    description: 'Chandler was one of the fathers of crime noire.'
-  });
-
-  // Save
-  console.log(await raymond.save());
-
-  // Update an existing author
-  let shakespeare = await Author.find(`.findOne({name: /Shake/})`);
-  shakespeare.description += ' And even greater...';
-  console.log(await shakespeare.save());
-
-  // Find and then delete Samuel Beckett
-  let samuel = await Author.find(`.findOne({name: /Beckett/})`);
-  if (samuel) {
-    console.log(await samuel.delete());
-  }
-}
-
-/* runTests(); */
-
-async function runMovieTest() {
-  let showtime = new Showtime({
-    audiotorium: "Stora Salongen",
-    film: "The Wife",
-    date: "2019-01-23",
-    time: "15:30"
-  });
-
-  // Save
-  console.log(await showtime.save());
-}
-
-runMovieTest();
-
-async function runTicketPriceTest() {
-  let ticketPrice = new Ticketprice({
-    normalFare: 85,
-    pensioners: 75,
-    children: 65
-    
-  });
-
-  // Save
-  console.log(await ticketPrice.save());
-}
-
-runTicketPriceTest();
-
-/*let time = new Showtime();
-
-time.createShowtimes();*/
