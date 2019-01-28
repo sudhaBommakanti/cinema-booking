c3f30e26ab42e3ea5ac8c584556ac18d5ebe6bdd
Sudha Parimala <mailme.sudha.k@gmail.com>
2019-01-28 11:25:34 +0100

    Login form

---
+++ www/js/components/Login.js
@@ -0,0 +1,15 @@
+class Login extends Component {
+
+    constructor(){
+      super();
+      this.addRoute('/login', 'Logga in');
+    }
+    static get baseRoute() {
+      return 'login/';
+    }
+  
+    async delete() {
+      this._id = 1;
+      return super.delete();
+    }
+}
