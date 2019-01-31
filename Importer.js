8e9b5c6c747da0955fadc95a450fba0ae8a51f57
Pontus Nilsson <p0nta_@hotmail.com>
2019-01-31 18:36:09 +0100

    Added movie importer to JSON and loaded.

--- Importer.js
+++ Importer.js
@@ -14,24 +14,40 @@ db.once('open', () => importJsonDataToDb());
 
 // Load the Mongoose model
 let Auditorium = require('./schemas/Auditorium');
+let Movie = require('./schemas/Movie');
 
 // Load the json data from file
 let auditoriumData = require('./auditorium.json');
+let movieData = require('./movie.json');
 
 async function importJsonDataToDb() {
-   let allAuditoriumCount = await Auditorium.count();
-   // if the db already contains books then delete them
-   if (allAuditoriumCount > 0) {
-       console.log('Deleted old auditoriums', await Auditorium.remove({}));
-   }
-   for (let data of auditoriumData) {
-       let auditorium = new Auditorium(data);
-       // save the book to MongoDB
-       await auditorium.save();
-   }
-   // after the import count the books again
-   allAuditoriumCount = await Auditorium.count();
-   console.log(`Imported ${allAuditoriumCount} auditoriums to the database`);
-   // Exit the app
-   process.exit();
+    let allAuditoriumCount = await Auditorium.count();
+    let allMovieCount = await Movie.count();
+    // if the db already contains books then delete them
+    if (allAuditoriumCount > 0) {
+        console.log('Deleted old auditoriums', await Auditorium.remove({}));
+    }
+
+    if (allMovieCount > 0) {
+        console.log('Deleted old movies', await Movie.removei({}));
+    }
+
+    for (let data of auditoriumData) {
+        let auditorium = new Auditorium(data);
+        // save the book to MongoDB
+        await auditorium.save();
+    }
+
+    for (let data of movieData) {
+        let movie = new Movie(data);
+        // save the book to MongoDB
+        await movie.save();
+    }
+    // after the import count the books again
+    allAuditoriumCount = await Auditorium.count();
+    allMovieCount = await Movie.count();
+    console.log(`Imported ${allAuditoriumCount} auditoriums to the database`);
+    console.log(`Imported ${allMovieCount} movies to the database`);
+    // Exit the app
+    process.exit();
 }
