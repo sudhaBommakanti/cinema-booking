ad0366e4bb94ac8c45d23574affb9951fa095a85
Joel <joel.pedersen97@gmail.com>
2019-02-04 14:37:40 +0100

    Added function to generate booking number - With Sudha

--- schemas/Booking.js
+++ schemas/Booking.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let bookingSchema = new Schema ({
-    "bookingNum":String,
-    "showTimeDetails":{type: Schema.Types.ObjectId, ref: 'Showtime'},
-    "seats":[String],
-    "userId":{type: Schema.Types.ObjectId, ref: 'User'}
+let bookingSchema = new Schema({
+  "bookingNum": String,
+  "showTimeDetails": { type: Schema.Types.ObjectId, ref: 'Showtime' },
+  "seats": [String],
+  "userId": { type: Schema.Types.ObjectId, ref: 'User' }
 })
+
+bookingSchema.pre('save', () => {
+  if (this.bookingNum) {
+    return
+  }
+  // write code here to generate a booking number
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVXYZ0123456789'
+  let uniqueId = '';
+
+  for (let i = 0; i < 5; i++) {
+    uniqueNum += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return this.bookingNum = uniqueId;
+});
