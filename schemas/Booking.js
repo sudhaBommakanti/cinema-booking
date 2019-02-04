96e0b831fd38061ae1d3da7292598f4789e452a0
Joel <joel.pedersen97@gmail.com>
2019-02-04 15:05:54 +0100

    Added code for checking if unique booking number - With Sudha

--- schemas/Booking.js
+++ schemas/Booking.js
@@ -7,20 +7,23 @@ let bookingSchema = new Schema({
   "seats": [String],
   "userId": { type: Schema.Types.ObjectId, ref: 'User' }
 })
-
-bookingSchema.pre('save', () => {
+// Here we add a pre save hook that will create a unique booking number
+bookingSchema.pre('save', async () => {
   if (this.bookingNum) {
     return
   }
   // write code here to generate a booking number
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVXYZ0123456789'
-  let uniqueId = '';
 
-  for (let i = 0; i < 5; i++) {
-    uniqueNum += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
+  do {
 
-  return this.bookingNum = uniqueId;
+    let uniqueId = '';
+    for (let i = 0; i < 5; i++) {
+      uniqueNum += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+  } while ((await models.users.find({ bookingNum: uniqueId })).length > 0)
+  this.bookingNum = uniqueId;
 });
 
 module.exports = db.model('Booking', bookingSchema);
