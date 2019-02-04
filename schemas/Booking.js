9864471e49ffcb1e4acfea21cca529e5b6cc8e2d
Sudha Parimala <mailme.sudha.k@gmail.com>
2019-02-04 13:50:44 +0100

    booking module export

--- schemas/Booking.js
+++ schemas/Booking.js
@@ -6,4 +6,6 @@ let bookingSchema = new Schema ({
     "showTimeDetails":{type: Schema.Types.ObjectId, ref: 'Showtime'},
     "seats":[String],
     "userId":{type: Schema.Types.ObjectId, ref: 'User'}
-})
+});
+
+module.exports = db.model('Booking', bookingSchema);
