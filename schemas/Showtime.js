33301d773fd501c8173763236c2fc6d843cfb59c
Joel <joel.pedersen97@gmail.com>
2019-02-06 15:10:01 +0100

    parprogg med pontus

--- schemas/Showtime.js
+++ schemas/Showtime.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let showtimeSchema = new Schema({
-    "auditorium": { type: Schema.Types.ObjectId, ref: 'Auditorium' },
-    //"film": {type: Schema.Types.ObjectId, ref: 'Movie'},
-    "film": String,
-    "date": String,
-    "time": String
+  "auditorium": { type: Schema.Types.ObjectId, ref: 'Auditorium' },
+  //"film": {type: Schema.Types.ObjectId, ref: 'Movie'},
+  "film": String,
+  "date": String,
+  "time": String
 });
 
 module.exports = db.model('Showtime', showtimeSchema);
--- www/js/components/Showtime.js
+++ www/js/components/Showtime.js
@@ -1,24 +1,23 @@
 class Showtime extends Component {
-    constructor(props) {
-        super(props)
-        this.addEvents({
-            'click .selectable': 'showAuditorium'
-        });
-        this.showAudi = false;
-        
-    }
+  constructor(props) {
+    super(props)
+    this.addEvents({
+      'click .selectable': 'showAuditorium'
+    });
+    this.showAudi = false;
 
-    async BarbieGirl(){
-        this.currentAudi = Showtime.find(`.find().populate('auditorium').exec()`);
-        console.log(this.currentAudi);
-        return this.currentAudi;
+  }
 
-    }
+  async BarbieGirl() {
+    this.currentAudi = await Showtime.find(`.find().populate('auditorium').exec()`);
+    console.log(this.currentAudi);
+    return this.currentAudi;
 
-    showAuditorium() {
-        this.showAudi = true;
+  }
 
-        this.render();
-    }
+  showAuditorium() {
+    this.showAudi = true;
+    this.render();
+  }
 
 }
