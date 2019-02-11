a331ad00507ef386767f9b4da98b48fda7af6128
Joel <joel.pedersen97@gmail.com>
2019-02-11 15:58:38 +0100

    Seats are bookable - parprogg

--- www/js/components/Auditorium.js
+++ www/js/components/Auditorium.js
@@ -1,17 +1,29 @@
 class Auditorium extends Component {
-    constructor(props) {
-        super(props);
-        this.seats = [];
-        let row = 1;
-        let seatNum = 1;
-        for (let numberOfSeatsInTheRow of this.seatsPerRow) {
-            let seatsInRow = [];
-            while (seatsInRow.length < numberOfSeatsInTheRow) {
-                seatsInRow.push(new Seat({row, seatNum}));
-                seatNum++;
-            }
-            this.seats.push(seatsInRow);
-            row++;
-        } 
+  constructor(props) {
+    super(props);
+    this.addEvents({
+      'click .seat': 'seatClick'
+    });
+    this.seats = [];
+    this.seatsBySeatNumber = {};
+    let row = 1;
+    let seatNum = 1;
+    for (let numberOfSeatsInTheRow of this.seatsPerRow) {
+      let seatsInRow = [];
+      while (seatsInRow.length < numberOfSeatsInTheRow) {
+        let seat = new Seat({ row, seatNum });
+        seatsInRow.push(seat);
+        this.seatsBySeatNumber[seatNum] = seat;
+        seatNum++;
+      }
+      this.seats.push(seatsInRow);
+      row++;
     }
+  }
+  seatClick(e) {
+    console.log(e.target);
+    let seat = this.seatsBySeatNumber[$(e.target).attr('data-seat')];
+    seat.booked = seat.booked ? false : true;
+    seat.render();
+  }
 }
