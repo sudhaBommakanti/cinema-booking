5bedea85b3f73ec5f8eab7895ae6760e056ea3ad
Joel <joel.pedersen97@gmail.com>
2019-02-20 23:25:23 +0100

    added validation for login and some styling

--- www/js/components/Login.js
+++ www/js/components/Login.js
@@ -1,24 +1,30 @@
 class Login extends Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
     this.addRoute('/login', 'Logga in');
-    
+
     this.addEvents({
       'click .submit-btn': 'login',
 
       'click .register-btn': 'register'
     })
     this.loggedIn = false;
+    this.createAccount = false;
   }
   static get baseRoute() {
     return 'login/';
   }
- 
+
   /**
   *
   * To go back to the start page after logged out and loads the web browser
   *
   */
+
+  mount() {
+    this.createAccount = false;
+    this.render();
+  }
 
   async delete() {
     this._id = 1;
@@ -27,19 +33,33 @@ class Login extends Component {
     Router.goto('/');
     location.reload()
 
-    
+
   }
- 
+
   async register() {
     let user = new User({
       email: $('.email').val(),
       password: $('.password').val()
     });
-    await user.save();
-    $('.email').val('')
-    $('.password').val('')
+    let validateEmailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let validatePasswordRegEx = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    if (validateEmailRegEx.test(user.email)) {
+      if (validatePasswordRegEx.test(user.password) === false) {
+        $(this.baseEl).find('#registerModalPassword').modal({ show: true });
+        return;
+      }
+      await user.save();
+      $('.email').val('')
+      $('.password').val('')
+      this.createAccount = true;
+      this.render();
+      return;
+    } else {
+      $(this.baseEl).find('#registerModalEmail').modal({ show: true });
+    }
   }
- 
+
   async login() {
     let login = new Login({
       email: $('.email').val(),
@@ -48,6 +68,7 @@ class Login extends Component {
     let result = await login.save();
     if (result.error) {
       alert(result.error);
+      return;
     }
     App.app.checkIfLoggedIn();
     this.loggedIn = true;
@@ -59,4 +80,4 @@ class Login extends Component {
     */
     Router.goto('/mybookings');
   }
- }
+}
