030c8e509041c3883f174c91979455db81b87747
Fanny <fanny_andersson@hotmail.com>
2019-02-20 11:48:11 +0100

    Add movieinfo-style

--- www/js/components/Movieinfo.js
+++ www/js/components/Movieinfo.js
@@ -2,15 +2,24 @@ class MovieInfo extends Component {
     constructor(props) {
         super(props);
         this.addRoute(/film\/(.*)/);
+        this.addEvents({
+            'click .buttons-div': 'openTrailerModal'
+        });
     }
 
     async mount() {
-        console.log('HEJ');
         // Get the id from the route
         let id = this.getRouteParts()[0];
         // Get the movie with that id
         this.movie = await Movie.find(id);
-        console.log(this.movie.image);
         this.render();
+    }
+
+    openTrailerModal() {
+        console.log('hehahaehah');
+        $(this.baseEl)
+            .find('#modal-trailer')
+            .modal({ show: true });
+        console.log('hej');
     }
 }
