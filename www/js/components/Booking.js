77905e36f25500af870b8812f57ab2da451615d4
Fanny <fanny_andersson@hotmail.com>
2019-02-21 12:18:57 +0100

    Auto stash before merge of "dev-refaktorering" and "origin/dev-refaktorering"

--- www/js/components/Booking.js
+++ www/js/components/Booking.js
@@ -1,24 +1,23 @@
 class Booking extends Component {
-	constructor(props) {
-		super(props)
-		this.addRoute('/showtime', 'Showtimes');
-		this.movies = [];
-		this.getShowtimes();
-	}
+    constructor(props) {
+            super(props)
+            this.addRoute('/showtime', 'Showtimes');
+            this.movies = [];
+            this.getShowtimes();
+        }
+        /**
+         *
+         * Gets all the movies from the DB and popluate showtimes for them
+         * Converts all showtime objects to real instances of Showtime
+         * Rerender the view to show the data we got from the DB
+         *
+         */
+    async getShowtimes() {
+        this.movies = await Movie.find(`.find().populate('showtimes').exec()`);
+        for (let movie of this.movies) {
+            movie.showtimes = movie.showtimes.map(x => new Showtime(x));
+        }
 
-	/**
-	*
-	* Gets all the movies from the DB and popluate showtimes for them
-	* Converts all showtime objects to real instances of Showtime
-	* Rerender the view to show the data we got from the DB
-	*
-	*/
-	async getShowtimes() {
-		this.movies = await Movie.find(`.find().populate('showtimes').exec()`);
-		for (let movie of this.movies) {
-			movie.showtimes = movie.showtimes.map(x => new Showtime(x));
-		}
-		
-		this.render();
-	}
+        this.render();
+    }
 }
